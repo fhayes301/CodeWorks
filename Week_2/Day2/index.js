18f4b2adba1461ddc89bc4658f39be906896c011
Codeworks User <fhayes30@gmail.com>
2018-07-19 15:20:11 -0700

    made some cool changes

---
+++ Week_2/Day2/index.js
@@ -0,0 +1,18 @@
+console.log("hello world")
+console.log(4)
+console.log(true)
+
+let greeting = "Hello World"
+console.log(greeting)
+
+console.log(3+4)
+console.log(800/32)
+console.log(2/3)
+
+let lovesCoffee = true;
+
+if ( !lovesCoffee ) {
+	console.log(`Drink some more!`);
+} else {
+	console.log(`HI`)
+}
